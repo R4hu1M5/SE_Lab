86d90bf827e146cbf6c9229816814b0aefbbccd4
Rahul Murali Shankar <rahulmurali.shankar@iiitb.org>
2019-08-09 16:29:58 +0530

    first commit

---
+++ javaAdd.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class javaAdd
+{
+  public static void main(String args[])
+  {
+    float a, b, res;
+    char choice, ch;
+    Scanner scan = new Scanner(System.in);
+    do
+    {
+      System.out.print("1. Add Two Numbers\n");
+      System.out.print("2. Multiply Two Numbers\n");
+      System.out.print("3. Exit\n\n");
+      System.out.print("Enter your choice: ");
+      choice = scan.next().charAt(0);
+      switch(choice)
+      {
+        case '1': System.out.print("Enter Two Numbers: ");
+                  a = scan.nextFloat();
+                  b = scan.nextFloat();
+                  res = a + b;
+                  System.out.print("Result = " + res);
+                  break;
+        case '3': System.exit(0);
+                  break;
+        default:  System.out.print("Invalid Choice");
+                  break;
+      }
+      System.out.print("\n-------------\n");
+    }
+    while (choice != 2);
+  }
+}
